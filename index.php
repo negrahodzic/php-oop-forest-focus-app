3850f7ade65a83939d7e114eb6266d807a4b0508
negrahodzic <negra.hodzic@gmail.com>
2020-10-31 10:12:31 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Forest</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="card text-center">
+        <div class="card-header"></div>
+        <div class="card-body">
+            <h5 class="card-title">Forest: Stay focused, be present!</h5>
+            <p class="card-text">Forest is an app that helps you stay focused on the important things in life.</p>
+        </div>
+        <div class="card-footer text-muted"></div>
+    </div>
+    <br><br><br>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4">
+                List od all tree species <br><br>
+                <ul class="list-group">
+                    <li class="list-group-item active">Tree</li>
+                    <li class="list-group-item">Name</li>
+                    <li class="list-group-item">Image</li>
+                    <li class="list-group-item">Description</li>
+                    <li class="list-group-item">Points</li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                Chosen tree <br><br>
+                <ul class="list-group">
+                    <li class="list-group-item active">Chosen Tree</li>
+                    <li class="list-group-item">Tree</li>
+                    <li class="list-group-item">Duration</li>
+                    <li class="list-group-item">Status</li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                Planting history <br><br>
+                <ul class="list-group">
+                    <li class="list-group-item active">Planting history </li>
+                    <li class="list-group-item">Tree</li>
+                    <li class="list-group-item">Number of Planted</li>
+                    <li class="list-group-item">Number of Withered</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
